bbef241b70a700c8a55bde758e984f86f0a86785
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-09-02 03:07:56 +0200

    managed to show some data

--- src/components/ContryDetails.js
+++ src/components/ContryDetails.js
@@ -1,13 +1,16 @@
 import { useParams } from "react-router-dom";
 
 
-const CountryDetails = ({Data})=>{
+const CountryDetails = (Data)=>{
     let {id } = useParams();
+    const {data} = Data;
 
+    const country = data.filter(country => country.alpha3Code === id )
+    console.log(country[0]);
     return(
-
         <div>
-            {id}
+            <h1>{country[0].name.common}</h1>
+            <h2>{country[0].region}</h2>
         </div>
     )
 }
